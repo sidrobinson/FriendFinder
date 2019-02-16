f48744d18425ab7341b7052c59eee773a91d7245
sidrobinson <sidneyjr.robinson@gmail.com>
2019-02-16 13:52:17 -0500

    Add files via upload

--- friendFinder.js
+++ friendFinder.js
@@ -2,53 +2,63 @@ let http = require("http");
 let fs = require("fs");
 
 // Server
-let server = http.createServer(routes);
+let PORT = 9000;
+const server = http.createServer(routes);
 
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end(" " + request.url);
+}
+
+// function for requests and responses 
 function routes(req, res) {
   const path = req.url;
+  console.log("hello");
+  console.log(path);
 
   switch (path) {
     case "/":
-      return home(req, res)
-    case "/matches":
-      return matches(req, res)
-    case "/profile":
-      return profile(req, res)
+      return function (req, res) {
+        console.log("hello from index.html route!");
+        fs.readFile(__dirname + "/index.html", function (err, data) {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+      }
+    case "/survey":
+      return function (req, res) {
+        fs.readFile(__dirname + "/survey.html", function (err, data) {
+          // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+      }
+
+    case "/apiFriendsList":
+      return function (req, res) {
+        fs.readFile(__dirname + "/apiFriendList.html", function (err, data) {
+          // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+
+        });
+      }
+
+  case "/gitHubRepo":
+      return function (req, res) {
+        fs.readFile(__dirname + "/gitHubRepo.html", function (err, data) {
+          // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+      }
+
   }
+
 }
-
-// function for requests and responses  
-function home(req, res) {
-
-  fs.readFile(__dirname + "/index.html", function (err, data) {
-
-    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
+  // Starts our server
+  server.listen(PORT, function () {
+    console.log("Server linstening ");
   });
-}
-
-function home(req, res) {
-
-  // fs package to read our index.html file
-  fs.readFile(__dirname + "/matches.html", function (err, data) {
-
-    // respond to the client with the HTML page by specifically telling the browser that we are delivering
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-
-  function home(req, res) {
-
-    // fs package to read our index.html file
-    fs.readFile(__dirname + "/profile.html", function (err, data) {
-
-      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-
-    // Starts our server
-    server.listen(PORT, function () {
-      console.log("Server is listening on PORT: " + PORT);
-    });
