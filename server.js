71899b64a4eeb43da855b8fdfc22667309fa9efd
sidrobinson <sidneyjr.robinson@gmail.com>
2019-02-21 19:12:55 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,33 +2,30 @@ var express = require("express");
 var path = require("path");
 
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
+// This sets up the basic properties the server
 
 // Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port. We"ll use this later in the listener
 var PORT = process.env.PORT || 1738;
 
-// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// express.json and express.urlEncoded make it easy for the server to interpret data sent to it.
 // The code below is pretty standard.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static('css'));
-
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(express.static("css"));
+app.use("/css", express.static(path.join(__dirname, "/css")));
 
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// The below points the server to a series of "route" files.
+// These routes give the server a "map" of how to respond when users visit or request data from various URLs.
 
 // require("./app/routing/apiRoutes")(app);
 // require("./app/routing/htmlRoutes")(app);
 
 // LISTENER
-// The below code effectively "starts" our server
-
+// The below code "starts" the server
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
